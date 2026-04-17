ac84a23b0795c2bbcaf4387029e1a27df3d1452c
Temilorun Olawuni <dev.temilorun@gmail.com>
2026-04-18 00:47:42 +0100

    Added some dummy project in my asset for my project scene

--- assets/asset.ts
+++ assets/asset.ts
@@ -5,3 +5,41 @@ export const assets = {
   menu_icon,
   close_icon,
 }
+
+export const projects = [
+  {
+    title: "Strands That Perfectly Complement You!",
+    status: "IN PROCESS",
+    tags: ["React", "Tailwind", "Node.js"]
+  },
+  {
+    title: "Yvonne Kafor Coaching",
+    status: "FEATURED",
+    description: "Experienced & Compassionate Mediator, Divorce & Custody Coach",
+    tags: ["JavaScript", "Express.js", "React", "Node.js", "CSS"]
+  },
+  {
+    title: "Confessio",
+    status: "FEATURED",
+    tagline: "Speak without fear.",
+    tags: ["HTML", "Tailwind", "PostgreSQL", "Express.js", "Socket.io"]
+  },
+  {
+    title: "Axelerate",
+    status: "FEATURED",
+    tagline: "DATA-DRIVEN SCALE HUMAN-DRIVEN DESIRE",
+    tags: ["Next.js", "TypeScript", "Tailwind"]
+  },
+  {
+    title: "LinguaCore AI",
+    status: "Live",
+    tagline: "Global Intent. Local Voices.",
+    tags: ["AI", "Python", "React Native"]
+  },
+  {
+    title: "Xtensionvrsse",
+    status: "Live",
+    tagline: "Global Intent. Local Voices.",
+    tags: ["Next.js", "GraphQL", "PostgreSQL"]
+  }
+];
