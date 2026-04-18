8cd553afb235a2f8e66eeadf92573909a938147c
Temilorun Olawuni <dev.temilorun@gmail.com>
2026-04-18 09:30:16 +0100

    Added the use client directive

--- scenes/Contact.tsx
+++ scenes/Contact.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+
 import Button from "@/components/Button";
 import  { useRouter } from "next/navigation"
 
--- scenes/Hero.tsx
+++ scenes/Hero.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Button from "@/components/Button"
 import  { useRouter } from "next/navigation"
 
